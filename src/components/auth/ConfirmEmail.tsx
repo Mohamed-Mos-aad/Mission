3568382a06a71ffcb6c76616571a6b9ae31937a7
Mohamed-Mos-aad <ma3268787@gmail.com>
2024-09-06 21:43:32 +0300

    Confirm email component logic Finished

--- src/components/auth/ConfirmEmail.tsx
+++ src/components/auth/ConfirmEmail.tsx
@@ -1,32 +1,235 @@
+import { useEffect, useRef, useState } from 'react';
 import checkCodeLogo from '../../assets/CheckCode.png'
+import successLogo from '../../assets/SuccessIcon.png'
 import style from '../../style/components/confirmEmail.module.css'
+import emailjs from '@emailjs/browser';
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+
+// **Interfaces
+interface ITime{
+    minutes: number,
+    seconds: number
+}
+
 
 
 
 
 export default function ConfirmEmail() {
+    // ** Defaults value
+    let currentMinutes = 2;
+    let currentSeconds = 0;
+    let timerInterval:NodeJS.Timeout;
+    const navigate = useNavigate();
+
+
+
+
+    // ** States
+    const [time,setTime] = useState<ITime>({
+        minutes: 2,
+        seconds: 0
+    })
+    const [inputsValues, setInputsValues] = useState<string[]>(['', '', '', '']);
+    const [userInputCode,setUserInputCode] = useState<string>('');
+    const [codeWrong,setCodeWrong] = useState<boolean>(false);
+    const [codeSended,setCodeSended] = useState<boolean>(false);
+    const [confirmCode,setConfirmCode] = useState<number|null>(null)
+    const [confirmEmailSuccess,setConfirmEmailSuccess] = useState<boolean>(false);
+
+
+
+    
+    // ** Ref
+    const inputsRef = useRef<HTMLInputElement[]>([]);
+
+    
+
+    
+
+    // ** Handlers
+    const nextInputHandler = (e: React.FormEvent<HTMLInputElement>)=>{
+        const target = e.target as HTMLInputElement;
+        const index = Number(target.id);
+        const isNumeric = /^[0-9]$/.test(target.value);
+        setInputsValues((prev)=>{
+            const updatedCode = [...prev]; 
+            updatedCode[index] = target.value
+            return updatedCode;
+        })
+
+        if(!isNumeric)
+        {
+            target.value = '';
+        }
+
+        if(index < 3 && target.value.length > 0)
+        {
+            const input = inputsRef.current[index+1];
+            input.focus();
+        }
+    }
+    
+    const timerStartHandler = ()=>{
+        if(currentSeconds === 0)
+        {
+            currentMinutes = currentMinutes - 1;
+            currentSeconds = 59;
+
+            setTime({
+                minutes: currentMinutes,
+                seconds: currentSeconds
+            });
+        }
+        else
+        {
+            currentSeconds = currentSeconds -1;
+            setTime({
+                minutes: currentMinutes,
+                seconds: currentSeconds
+            });
+        }
+
+        if(currentMinutes === 0 && currentSeconds === 0)
+        {
+            clearInterval(timerInterval);
+            currentMinutes = 2;
+            currentSeconds = 0;
+            setTime({
+                minutes: currentMinutes,
+                seconds: currentSeconds
+            });
+            const sendBtn = document.getElementById('sendBtn');
+            if(sendBtn)
+            {
+                sendBtn.innerHTML = 'Resend';
+                sendBtn.addEventListener('click',sendCodeHandler);
+                sendBtn.style.cursor = 'pointer';
+            }
+            setCodeSended(false);
+        }
+    }
+
+    const sendCodeHandler = ()=>{
+        const sendBtn = document.getElementById('sendBtn');
+        if(sendBtn)
+        {
+            sendBtn.removeEventListener('click',sendCodeHandler);
+        }
+        setCodeSended(true);
+    };
+
+
+    const userEmail = 'ma3268787@gmail.com';
+    const verificationCode = Math.floor(1000 + Math.random() * 9000);
+
+    const sendEmailHandler = ()=>{
+        setConfirmCode(verificationCode);
+        const serviceId = 'service_e75s08c';
+        const templateId = 'template_jsraclg';
+        const templateParams = {
+            user_email: userEmail,
+            user_name: 'mohamed',
+            message: verificationCode,
+        };
+        const publicKey = '3PaUw5fPoj59Hzt83';
+
+        emailjs.send(serviceId, templateId, templateParams , {publicKey});
+    }
+
+
+
+
+
+
+    // ** UseEffects
+    useEffect(()=>{
+        inputsRef.current = Array.from(document.querySelectorAll<HTMLInputElement>('input'));
+        const sendBtn = document.getElementById('sendBtn');
+        sendBtn?.addEventListener('click',sendCodeHandler);
+    },[]);
+    
+    useEffect(()=>{
+        if(codeSended)
+        {
+            timerInterval = setInterval(timerStartHandler, 1000);
+            sendEmailHandler();
+            const sendBtn = document.getElementById('sendBtn');
+            if(sendBtn)
+            {
+                sendBtn.style.cursor = 'no-drop';
+            }
+        }
+    },[codeSended]);
+
+    useEffect(()=>{
+        if (inputsValues.every(value => value !== ''))
+        {
+            const code = inputsValues.join('');
+            setUserInputCode(code)
+        }
+    },[inputsValues]);
+
+    useEffect(()=>{
+        if (userInputCode !== '')
+        {
+            if(userInputCode !== confirmCode?.toString())
+            {
+                setCodeWrong(true);
+            }
+            else
+            {
+                setConfirmEmailSuccess(true);
+            }
+        }
+    },[userInputCode]);
+
+
+
+
+
     return (
         <>
-            <div className={style.confirm_email_container}>
-                <div className={style.logo}>
-                    <img src={checkCodeLogo} alt="Check code logo" />
-                    <h2>Check Code</h2>
-                </div>
-                <div className={style.code}>
-                    <div className={style.code_input}>
-                        <input type="text" maxLength={1}/>
-                        <input type="text" maxLength={1}/>
-                        <input type="text" maxLength={1}/>
-                        <input type="text" maxLength={1}/>
+            {confirmEmailSuccess?
+                <div className={style.confirm_email_container}>
+                    <div className={style.success_logo}>
+                        <img src={successLogo} alt="Success logo" />
+                        <h2>Email has been Confirmed Successfully</h2>
                     </div>
-                    <div className={style.timer}>
-                        01:55
+                    <div className={style.btn_container}>
+                        <button onClick={()=>{navigate('/login')}}>Login</button>
                     </div>
                 </div>
-                <div className={style.btn_container}>
-                    <button>Send</button>
+            :
+                <div className={style.confirm_email_container}>                    
+                    <div className={style.logo}>
+                        <img src={checkCodeLogo} alt="Check code logo" />
+                        <h2>Check Code</h2>
+                    </div>
+                    <div className={style.code}>
+                        <div className={style.code_input}>
+                            <input type="text" maxLength={1} id='0' onInput={(e)=>{nextInputHandler(e)}}/>
+                            <input type="text" maxLength={1} id='1' onInput={(e)=>{nextInputHandler(e)}}/>
+                            <input type="text" maxLength={1} id='2' onInput={(e)=>{nextInputHandler(e)}}/>
+                            <input type="text" maxLength={1} id='3' onInput={(e)=>{nextInputHandler(e)}}/>
+                        </div>
+                        <div className={style.timer}>
+                            0{time.minutes}:{time.seconds > 9 ? time.seconds : `0${time.seconds}`}
+                        </div>
+                        <div className={style.code_error}>
+                            <h3>{codeWrong ? "Wrong code" : " "}</h3>
+                        </div>
+                    </div>
+                    <div className={style.btn_container}>
+                        <button id='sendBtn'>Send</button>
+                    </div>
                 </div>
-            </div>
+            }
+            
         </>
     )
 }
