1db30d7cc9513d400556b18851c2451c1f281f20
Mohamed-Mos-aad <ma3268787@gmail.com>
2024-11-01 16:50:53 +0200

    Welcome Page Finished

--- src/pages/auth/Welcome.tsx
+++ src/pages/auth/Welcome.tsx
@@ -1,7 +1,43 @@
+import { useNavigate } from 'react-router-dom'
+import style from '../../style/pages/auth/welcome.module.css'
+
+
+
+
+
 export default function Welcome() {
+    // ** States
+    const navigate = useNavigate();
+
+
+
+
+
+    // ** Handlers
+    const openSignUpPageHandler = ()=>{
+        navigate('/registration')
+    }
+    const openLogInPageHandler = ()=>{
+        navigate('/login')
+    }
+
+
+
+
+
     return (
         <>
-            Welcome
+            <div className={style.welcome_conatiner}>
+                    <p>
+                        Welcome <br />
+                        to Mission <br />
+                        app
+                    </p>
+                    <div className={style.welcome_btns}>
+                        <button onClick={openSignUpPageHandler}>Sign UP</button>
+                        <button onClick={openLogInPageHandler}>Log In</button>
+                    </div>
+            </div>
         </>
     )
 }
