98fecca667ea8b3a5f429c89e7854070f091bb35
Mohamed-Mos-aad <ma3268787@gmail.com>
2024-11-27 11:20:58 +0200

    Edite firebase data

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDlR5fTLFcRMmMH2CkxBuHfQh8-d1l3F1o",
-  authDomain: "mission-00001.firebaseapp.com",
-  projectId: "mission-00001",
-  storageBucket: "mission-00001.firebasestorage.app",
-  messagingSenderId: "956696511029",
-  appId: "1:956696511029:web:0a89fd95c1edd84160e77c",
-  measurementId: "G-QXPPLNWKT3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID_ID
 };
